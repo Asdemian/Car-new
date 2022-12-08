de511e1e6e5f5fe8e10d8e2d0ccd9f28cd7afe33
hw commit <demaniks@yahoo.com>
2022-12-08 16:23:32 +0300

    ДЗ 2-2

--- src/Car.java
+++ src/Car.java
@@ -7,6 +7,15 @@ public class Car {
     int productionYear;
     String productionCountry;
 
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
     @Override
     public String toString() {
         return brand+ " "+model+ " , " +productionYear+ " год выпуска, сборка в "
--- src/Main.java
+++ src/Main.java
@@ -3,46 +3,25 @@ import java.util.concurrent.Callable;
 public class Main {
     public static void main(String[] args) {
 
-        Car lada = new Car();
-        lada.brand = "Lada";
-        lada.model = "Grande";
-        lada.productionCountry = "России";
-        lada.engineVolume = 1.7;
-        lada.color = "Желтого";
-        lada.productionYear = 2015;
+        Car lada = new Car("Lada", "Grande", 1.7,
+                "Желтого", 2015,
+                "России");
 
-        Car audi = new Car();
-        audi.brand = "Audi";
-        audi.model = "A8 50 L TDI quattro";
-        audi.productionCountry = "Германии";
-        audi.engineVolume = 3.0;
-        audi.color = "черный";
-        audi.productionYear = 2020;
+        Car audi = new Car("Audi", "A8 50 L TDI quattro",
+                3.0, "черный", 2020,
+                "Германии");
 
-        Car bmw = new Car();
-        bmw.brand = "BMW";
-        bmw.model = "Z8";
-        bmw.productionCountry = "Германии";
-        bmw.engineVolume = 3.0;
-        bmw.color = "черный";
-        bmw.productionYear = 2021;
+        Car bmw = new Car("BMW", "Z8", 3.0,
+                "черный", 2021,
+                "Германии");
 
-        Car kia = new Car();
-        kia.brand = "Kia";
-        kia.model = "Sportage 4 поколение";
-        kia.productionCountry = "Южной Корее";
-        kia.engineVolume = 2.4;
-        kia.color = "красный";
-        kia.productionYear = 2018;
+        Car kia = new Car("Kia", "Sportage 4 поколение",
+                2.4, "красный", 2018,
+                "Южной Корее");
 
-        Car hyundai = new Car();
-        hyundai.brand = "Hyundai";
-        hyundai.model = "Avante";
-        hyundai.productionCountry = "Южной Корее";
-        hyundai.engineVolume = 1.6;
-        hyundai.color = "оранжевый";
-        hyundai.productionYear = 2016;
-
+        Car hyundai = new Car("Hyundai", "Avante",
+                1.6, "оранжевый", 2016,
+                "Южной Корее");
 
         System.out.println(lada);
         System.out.println(audi);
